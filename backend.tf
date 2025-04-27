1c12e7365266f338b8ec01738a2800cdaf94b37f
Akash <56023730+akash66sheoran@users.noreply.github.com>
2025-04-28 00:38:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "tod-tf-state-store"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    workspace_key_prefix = "tf-linux-ec2-workspaces"
-#     dynamodb_table = "terraform-state-lock"
-   }
- }
+
