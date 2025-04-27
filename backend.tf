ef2ed6d47e1633f54893d7fdb2f007a87769750e
Akash <56023730+akash66sheoran@users.noreply.github.com>
2025-04-28 01:07:50 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "tod-tf-state-store"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
